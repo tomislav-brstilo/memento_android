637f1651fd88ab7a8c2fea7f6e9acc9bd0efdcca
TRI-M\tbrstilo <tomislav.brstilo@tri-m.hr>
2025-09-23 14:44:51 +0200

    Post view improved

--- app/src/main/java/com/tomo/memento/PostDetailActivity.kt
+++ app/src/main/java/com/tomo/memento/PostDetailActivity.kt
@@ -6,6 +6,7 @@ import com.bumptech.glide.Glide
 import com.tomo.memento.databinding.ActivityPostDetailBinding
 
 class PostDetailActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityPostDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,15 +14,34 @@ class PostDetailActivity : AppCompatActivity() {
         binding = ActivityPostDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Retrieve extras
         val imageUrl = intent.getStringExtra("imageurl")
         val caption = intent.getStringExtra("caption")
+        val user = intent.getStringExtra("user")
+        val timestamp = intent.getStringExtra("timestamp")
 
-        // Load image
+        // Load photo (Glide recommended)
         Glide.with(this)
             .load(imageUrl)
             .into(binding.postImageView)
 
-        binding.captionTextView.text = caption ?: ""
+        // Set caption
+        binding.postCaptionTextView.text = "$caption"
+
+        // Example click listeners
+        binding.likeButton.setOnClickListener {
+            // TODO: Add logic
+        }
+        binding.commentButton.setOnClickListener {
+            // TODO: Add logic
+        }
+        binding.shareButton.setOnClickListener {
+            // TODO: Add logic
+        }
+        binding.saveButton.setOnClickListener {
+            // TODO: Add logic
+        }
+
+        // TODO: Populate comments dynamically (RecyclerView recommended)
     }
 }
+
